16ade9af844c289d5da781a247aab13af4fc8dba
kangsiwoo <bssmkangsiwoo@gmail.com>
2023-04-07 03:21:13 +0900

    JavaScript 1.4

---
+++ 1.4/index.js
@@ -0,0 +1,16 @@
+// const mon = "mon ";
+// const tue = "tue ";
+// const wed = "wed ";
+// const thu = "thu ";
+// const fri = "fri ";
+// const sat = "sat ";
+// const sun = "sun";
+
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+// Get Item from Array
+console.log(daysOfWeek, daysOfWeek[4]);
+
+// Add one more day to the Array
+daysOfWeek.push("sun")
+console.log(daysOfWeek);
