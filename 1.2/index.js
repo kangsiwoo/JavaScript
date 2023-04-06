74cae211357ad5460992be38321d62a4ce67201a
kangsiwoo <bssmkangsiwoo@gmail.com>
2023-04-07 02:57:02 +0900

    JavaScript 1.2

---
+++ 1.2/index.js
@@ -0,0 +1,12 @@
+const a = 5;
+const b = 2;
+let myName = "Kang Siwoo"
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello " + myName)
+
+myName = "Siwoo"
+
+console.log("your new name is " + myName)
